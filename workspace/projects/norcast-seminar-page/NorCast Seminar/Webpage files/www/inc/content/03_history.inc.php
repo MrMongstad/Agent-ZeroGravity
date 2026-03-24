1a0fb7eaf65ff51552716293e456dbfd4b2ac42e
The Night Watch <antigravity@nightwatch.local>
2026-03-24 20:57:22 +0100

    fix: clamp committee profile images & cleanup history page

--- workspace/projects/norcast-seminar-page/NorCast Seminar/Webpage files/www/inc/content/03_history.inc.php
+++ workspace/projects/norcast-seminar-page/NorCast Seminar/Webpage files/www/inc/content/03_history.inc.php
@@ -6,29 +6,31 @@
             <h2>History of NorCast</h2>
           </header>
           <span class="image centered"><img src="images/history.jpg" /></span>
-					<p>
-						<strong>The NorCast Conference concept derived from the need for a forum for casthouse actualities and related products in the Aluminium Industry, in general.
-						</strong></p>
-					<p><strong>
-						NorCast is a unique concept, and the brain child of Mr Arne Schaathun, whose long career and experience in the aluminium industry has given him both significant technical know-how and business
-						acumen. The NorCast Conference Committee Members are senior professionals with broad experience from the aluminium industry, and set the Conference program to ensure it reflects current casthouse
-						challenges, trends and developments.
-						</strong></p>
-					<p><strong>
-						The setting provides an intimate, and second to none, platform on which aluminium processors and producers can meet and gauge present and future opportunities, as well as challenges. It also offers
-						an excellent opportunity to meet with invited suppliers to the same industry.
-						</strong></p>
-					<p><strong>
-						The first NorCast Conference was held in 2006. The Concept limits the number of attendees to 120 executives, managers and professionals from the aluminium industry and casthouses. The Conference
-						Committee's excellent support has proven invaluable to the success of NorCast and delegates from approx. 20 different countries worldwide attend the conference in Arendal.
-						</strong></p>
-					<p><strong>
-						The topics presented range from Interface between Pot-room and Casthouse, various metal quality aspects, Safety, Critical Equipment, Energy and Recycling, to Aluminium Downstream up-dates. Each morning
-						starts with a Safety presentation.
-						<br>
-						The conferences in 2021 and 2022 had a high focus on: Recycling, "Green aluminum", "low CO2 footprint" and sustainabillity.
-						</strong><br/><br/><br/>
-					</p>
+					<div class="font-bold pb-2">
+						<p>
+							The NorCast Conference concept derived from the need for a forum for casthouse actualities and related products in the Aluminium Industry, in general.
+						</p>
+						<p>
+							NorCast is a unique concept, and the brain child of Mr Arne Schaathun, whose long career and experience in the aluminium industry has given him both significant technical know-how and business
+							acumen. The NorCast Conference Committee Members are senior professionals with broad experience from the aluminium industry, and set the Conference program to ensure it reflects current casthouse
+							challenges, trends and developments.
+						</p>
+						<p>
+							The setting provides an intimate, and second to none, platform on which aluminium processors and producers can meet and gauge present and future opportunities, as well as challenges. It also offers
+							an excellent opportunity to meet with invited suppliers to the same industry.
+						</p>
+						<p>
+							The first NorCast Conference was held in 2006. The Concept limits the number of attendees to 120 executives, managers and professionals from the aluminium industry and casthouses. The Conference
+							Committee's excellent support has proven invaluable to the success of NorCast and delegates from approx. 20 different countries worldwide attend the conference in Arendal.
+						</p>
+						<p>
+							The topics presented range from Interface between Pot-room and Casthouse, various metal quality aspects, Safety, Critical Equipment, Energy and Recycling, to Aluminium Downstream up-dates. Each morning
+							starts with a Safety presentation.
+						</p>
+						<p class="mb-2">
+							The conferences in 2021 and 2022 had a high focus on: Recycling, "Green aluminum", "low CO2 footprint" and sustainabillity.
+						</p>
+					</div>
         </article>
 			</div>
 		</div>
