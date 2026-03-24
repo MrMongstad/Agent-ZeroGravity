05953b2265ba52cade0c32a9799985fb6a0600d6
The Night Watch <antigravity@nightwatch.local>
2026-03-24 20:49:19 +0100

    style: remove inline styles on 10_contact in favor of CSS utility classes

--- workspace/projects/norcast-seminar-page/NorCast Seminar/Webpage files/www/inc/content/10_contact.inc.php
+++ workspace/projects/norcast-seminar-page/NorCast Seminar/Webpage files/www/inc/content/10_contact.inc.php
@@ -5,23 +5,23 @@
           <header>
             <h2>Contact NorCast</h2>
           </header>
-          <h2 style="font-size: 1.4em;">Conference Administration</h2>
-						<p style="font-size: 1.2em;">
+          <h2 class="fs-1-4em">Conference Administration</h2>
+						<p class="fs-1-2em mb-2">
 							<img src="images/committee/arne.jpg" /><br />
 							Mr Arne Schaathun <br> 
 							Founder &amp; Chairman <br>
 							Committee member<br /><br />
-							Phone: <a href="tel:+47 909 10 336" style="text-decoration: none;">+47 909 10 336</a><br />
-							Email: <a href="mailto:norcast@online.no" style="text-decoration: none;">norcast@online.no</a><br/>
-            </p><br />
+							Phone: <a href="tel:+47 909 10 336" class="no-underline">+47 909 10 336</a><br />
+							Email: <a href="mailto:norcast@online.no" class="no-underline">norcast@online.no</a><br/>
+            </p>
 			
-			<p style="font-size: 1.2em;">
+			<p class="fs-1-2em mb-2">
 							<img src="images/committee/svein_terje.png" /><br />
 							Mr Svein Terje Strandlie <br> NorCast Coordinator <br> 
 							Committee member <br /><br />
-							Phone: <a href="tel:+47 901 01 340" style="text-decoration: none;">+47 901 01 340</a><br />
-							Email: <a href="mailto:norcast@online.no" style="text-decoration: none;">norcast@online.no</a><br/>
-            </p><br />
+							Phone: <a href="tel:+47 901 01 340" class="no-underline">+47 901 01 340</a><br />
+							Email: <a href="mailto:norcast@online.no" class="no-underline">norcast@online.no</a><br/>
+            </p>
 						
         </article>
 			</div>
