105ac92751504907c510049856b6c66f5c59a29c
The Night Watch <antigravity@nightwatch.local>
2026-03-24 20:45:05 +0100

    style: extract inline styles and format 02_about page

--- workspace/projects/norcast-seminar-page/NorCast Seminar/Webpage files/www/inc/content/02_about.inc.php
+++ workspace/projects/norcast-seminar-page/NorCast Seminar/Webpage files/www/inc/content/02_about.inc.php
@@ -6,19 +6,22 @@
             <h2>About NorCast</h2>
           </header>
 					<span class="image centered"><img src="images/about_conference.jpg" /></span>
-					<p><strong>
+					<div class="font-bold pb-2">
+						<p>
 						NorCast is an Aluminium Casthouse Conference with over 20 presentations on casthouse actualities, and runs for one and a half days. Each year NorCast has had the pleasure of presenting high-profile
 						executive leaders as Distinguished Guest Speakers, along with an international line-up of presenters, to cover important aspects of the aluminium industry. 
-						<br><br>
+						</p>
+						<p>
 						The conference is based on invitation only and limited to 130 delegates attending in Arendal, Norway.
 						The attendees do normally come from approx. 20 different countries. 
-						<br>
-						However, some additional delegates will formally Register to attend virtually. <br>
-						<br>
+						</p>
+						<p>
+						However, some additional delegates will formally Register to attend virtually.
+						</p>
+						<p class="mb-2">
 						The social events during the conference include an informal Welcome Event on the arrival day afternoon/evening, and a traditional boat trip and the NorCast Annual Dinner. 
-						<br/><br/><br/>
-						
-						</strong></p>
+						</p>
+					</div>
         </article>
 			</div>
 		</div>
